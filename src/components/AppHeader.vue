d1f334156bb61796c5e612eccc3b9efeebbba911
federicaarosio <arosio.fede@gmail.com>
2024-03-30 17:22:01 +0100

    added style to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,10 +2,10 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-2 mx-auto d-flex justify-content-between text-center custom-nav">
+        <div class="col-8 col-md-3 mx-auto d-flex justify-content-evenly text-center custom-nav text-center custom-mb-big mt-5">
             <div v-for="(link, index) in linkItems" :key="index" >
-                <router-link :to="{ name: link.routeName }" class="custom-menu-item">
-                    {{ link.label }}
+                <router-link :to="{ name: link.routeName }" :class="{ 'active': $route.name === link.routeName }" class="custom-menu-item">
+                    <span>{{ link.label }}</span>
                 </router-link>
             </div>
         </div>
@@ -41,12 +41,21 @@ export default {
 <style lang="scss" scoped>
 .custom-nav {
     background-color: #f3f2f1;
+    border-radius: 35px;
+    padding: 0.9rem  0;
 
     .custom-menu-item {
         background-color: #f3f2f1;
         text-decoration: none;
         color: #666666;
+        font-weight: 500;
+        padding: 0.8rem 1.6rem;       
+    }
 
+    .active {
+        background-color: white;
+        padding: 0.8rem 1.6rem;
+        border-radius: 35px;
     }
 }
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,7 +2,7 @@
     <section>
         <div class="container">
             <div class="row">
-                <div class="col-6 mx-auto">
+                <div class="col-12 col-md-6 mx-auto">
                     <h1 class="text-center custom-fs-big">
                         A brand and product designer working with clients globally
                     </h1>

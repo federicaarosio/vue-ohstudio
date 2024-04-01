15c9a2961cb8906c1fde2915c5abb299d815c578
federicaarosio <arosio.fede@gmail.com>
2024-04-01 22:06:47 +0200

    added style to navigation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,61 +1,65 @@
 <template lang="">
 
-<div class="container">
-    <div class="row">
-        <div class="col-8 col-md-3 mx-auto d-flex justify-content-evenly text-center custom-nav text-center custom-mb-big mt-5">
-            <div v-for="(link, index) in linkItems" :key="index" >
-                <router-link :to="{ name: link.routeName }" :class="{ 'active': $route.name === link.routeName }" class="custom-menu-item">
-                    <span>{{ link.label }}</span>
-                </router-link>
+    <div class="container">
+        <div class="row">
+            <div class="col-10 col-md-3 mx-auto d-flex justify-content-between text-center custom-nav custom-mb-big mt-5">
+                <div v-for="(link, index) in linkItems" :key="index" class="text-center" >
+                    <router-link :to="{ name: link.routeName }" :class="{ 'active': $route.name === link.routeName }" class="custom-menu-item">
+                        <span>{{ link.label }}</span>
+                    </router-link>
+                </div>
             </div>
         </div>
     </div>
-</div>
+        
     
-
-
-</template>
-<script>
-export default {
-    data() {
-        return {
-            linkItems: [
-                {
-                    label: 'Home',
-                    routeName: 'home'
-                },
-                {
-                    label: 'About',
-                    routeName: 'about'
-                },
-                {
-                    label: 'Contact',
-                    routeName: 'contact'
-                },
-            ]
-        }
-    },
-}
-</script>
-
-<style lang="scss" scoped>
-.custom-nav {
-    background-color: #f3f2f1;
-    border-radius: 35px;
-    padding: 0.9rem  0;
-
-    .custom-menu-item {
-        background-color: #f3f2f1;
-        text-decoration: none;
-        color: #666666;
-        font-weight: 500;
-        padding: 0.8rem 1.6rem;       
+    
+    </template>
+    <script>
+    export default {
+        data() {
+            return {
+                linkItems: [
+                    {
+                        label: 'Home',
+                        routeName: 'home'
+                    },
+                    {
+                        label: 'About',
+                        routeName: 'about'
+                    },
+                    {
+                        label: 'Contact',
+                        routeName: 'contact'
+                    },
+                ]
+            }
+        },
     }
-
-    .active {
-        background-color: white;
-        padding: 0.8rem 1.6rem;
+    </script>
+    
+    <style lang="scss" scoped>
+    @use '../style/partials/variables' as *;
+    
+    .custom-nav {
+        background-color: $primary-color;
         border-radius: 35px;
+        padding: 0.9rem 0.25rem;
+    
+        .custom-menu-item {
+            // background-color: $primary-color;
+            text-decoration: none;
+            color: $text-color;
+            font-weight: 500;
+            padding: 0.8rem 1.6rem;
+            border-radius: 35px;
+            transition: background-color 0.5s;
+        }
+    
+        .active {
+            background-color: white;
+            padding: 0.8rem 1.6rem;
+            border-radius: 35px;
+        }
     }
-}
-</style>
+    </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,12 +8,30 @@
                     </h1>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-12 mx-auto d-flex justify-content-center align-items-center custom-fs-small">
+                    <div class="me-3 text-center">
+                        Expertise
+                    </div>
+                    <div v-for='expertise in expertiseList' class="custom-badge me-3 text-center">
+                        {{ expertise }}
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
 <script>
 export default {
-    name: 'HomePage'
+    name: 'HomePage',
+    data() {
+        return {
+            expertiseList:
+            [
+                'Branding', 'Product', 'Design Systems'
+            ]
+        }
+    },
 }
 </script>
 <style lang="">

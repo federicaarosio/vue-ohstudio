fc7f602d257500fa2110850eaec924990903db26
federicaarosio <arosio.fede@gmail.com>
2024-04-03 19:11:58 +0200

    added footer

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
 
   <!-- qui quello che cambia in base alle rotte -->
   <router-view></router-view>
+
+  <AppFooter />
 </template>
 
 <script>
 import Appheader from './components/AppHeader.vue';
-
+import AppFooter from './components/AppFooter.vue';
 export default {
   name: 'App',
   components: {
     Appheader,
+    AppFooter
   }
 }
 </script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,51 @@
+<template lang="">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-between align-items-center mb-4">
+                <div class="copyright">&#169; 2024</div>
+                <div class="socials-links d-flex">
+                    <a v-for="social in socialLinks" :href="social.url">{{ social.socialName }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppFooter',
+    data() {
+        return {
+            socialLinks: [
+                {
+                    socialName: 'Facebook',
+                    url: '#',
+                },
+                {
+                    socialName: 'Linkedin',
+                    url: '#',
+                },
+                {
+                    socialName: 'Instagram',
+                    url: '#',
+                },
+            ]
+        }
+    },
+}
+</script>
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+.socials-links {
+    gap: 10px;
+
+    a {
+        color: $text-color;
+        text-decoration: none;
+
+        &:hover {
+            color: #999999;
+        }
+    }
+}
+</style>
